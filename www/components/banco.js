bb93a4f1dcd03106d64d686b243790cde2af1d5b
DjRogerin <brenolins100@gmail.com>
2018-11-02 03:11:33 +0900

    Finalizando botão deletar, alteração dos campos e salvando

--- www/components/banco.js
+++ www/components/banco.js
@@ -48,22 +48,23 @@ function preencheCarros(){
     });
 }
 
-$(document).on("change", "#lista", function(){
-	var codigoSelecionado = $("option:selected", ("#listar")).val();
-	$.ajax({
-	type:"get",
-	url:"https://crud-carros-linss2.c9users.io/listar1carro.php",
-	data:"codigo="+codigoSelecionado,
-	dataType:"json",
-	success: function(data){
-		$.each(data.carros, function(i, dados){
-		$("#modelo").val(dados.modelo), 
-		$("#cor").val(dados.cor),
-		$("#fabricante").val(dados.fabricante),
-		$("#ano").val(dados.ano),
-		$("#valor").val(dados.valor)
-		});
-	},
+  $(document).on("change", "#lista", function(){
+    var codigoSelecionado = $("option:selected", ("#lista")).val();
+    $.ajax({
+    type:"get",
+    url:"https://crud-carros-linss2.c9users.io/listar1carro.php",
+    data:"codigo="+codigoSelecionado,
+    dataType:"json",
+    success: function(data){
+      $.each(data.carros, function(i, dados){
+      $("#codigo").val(dados.codigo), 
+      $("#modelo").val(dados.modelo), 
+      $("#cor").val(dados.cor),
+      $("#fabricante").val(dados.fabricante),
+      $("#ano").val(dados.ano),
+      $("#valor").val(dados.valor)
+      });
+    },
 	error: function(data){
 		navigator.notification.alert("Erro: "+data);
 	}
@@ -75,45 +76,68 @@ $(document).on("click","#btnReturn", function(){
   $(location).attr("href","index.html");
 });
 
-//botao excluir
-$(document).on("click", "#delete", function(){
-	$.ajax({
-	  type:"get",
-	  url:"https://crud-carros-linss2.c9users.io/deletarcarro.php",
-	  data:"codigo="+$("#codigo").val(),
-	  success: function(data){
-	    navigator.notification.alert(data);
-	    location.reload();
-	   },
-	   error:function(data){
-	     navigator.notification.alert("Erro: "+data);
-	   }
-	});
-	
+$(document).on("click","#delete", function(){
+    var codigoSelecionado = $("option:selected", ("#lista")).val();
+    $.ajax({
+      type:"get",
+      url:"https://crud-carros-linss2.c9users.io/deletarcarro.php",
+      data:"codigo="+codigoSelecionado,
+       success: function(data){
+            navigator.notification.alert(data);
+            location.reload();
+        },
+        error:function(data){
+          navigator.notification.alert("erro: "+data);
+        }
+    });
+});
+//botao alterar
+$(document).on("click","#btnSalva", function(){
+    var parametros = {
+        "codigo": $("#codigo").val(),
+        "modelo": $("#modelo").val(),
+        "cor": $("#cor").val(),
+        "fabricante": $("#fabricante").val(),
+        "ano": $("#ano").val(),
+        "valor": $("#valor").val()
+
+    };
+    $.ajax({
+        type:"post",
+        url:"https://crud-carros-linss2.c9users.io/alterar.php",
+        data: parametros,
+        success: function(data){
+          navigator.notification.alert(data);
+          location.reload();
+        },
+        error:function(data){
+          navigator.notification.alert("erro: "+data);
+        }
+    });
 });
 
-//botao que salva as alterações no banco
-$(document).on("click", "#btnSalva", function(){
-	var parametros = {
-	  "codigo":$("#codigo").val(), 
-    "modelo":$("#modelo").val(), 
-    "cor":$("#cor").val(), 
-    "fabricante":$("#fabri").val(), 
-    "ano":$("#codigo").val(), 
-    "valor":$("#codigo").val(), 
-    
-	};
-	$.ajax({
-    type:"post",
-		url:"https://crud-carros-linss2.c9users.io/alterar.php",
-		data:parametros,
-		success: function(data){
-			navigator.notification(data);
-			location.reload();
-		},
-		erro:function(data){
-			navigator.notification.alert("Erro: "+data);
-		}
-	});
+$(document).on("click","#editar", function(){
+    habilita();
 });
-
+$(document).on("click","#cancelar", function(){
+   desabilita();
+    $("#modelo").val("");
+          $("#cor").val("");
+          $("#fabricante").val("");
+          $("#ano").val("");
+          $("#valor").val("");
+});
+function desabilita(){
+    $("#modelo").prop('readonly',true);
+    $("#cor").prop('readonly',true);
+    $("#fabricante").prop('readonly',true);
+    $("#ano").prop('readonly',true);
+    $("#valor").prop('readonly',true);
+}
+function habilita(){
+      $("#modelo").prop('readonly',false);
+    $("#cor").prop('readonly',false);
+    $("#fabricante").prop('readonly',false);
+    $("#ano").prop('readonly',false);
+    $("#valor").prop('readonly',false);
+}
